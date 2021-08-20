c3be717cd2228da0c97f3d4c4ff2d4b56b3dd5b6
Сергей Ростокин <rostockin.serg@yandex.ru>
2021-08-20 20:16:22 +0300

    добавил новый код

--- ProjectRogalik/ProjectRogalik.cpp
+++ ProjectRogalik/ProjectRogalik.cpp
@@ -16,20 +16,57 @@ void gotoxy(int x, int y)
 //класс
 class CHero
 {
-
 private:
 	int x, y; //координаты положения персонажа на экране
 	int face; // отображение персонажа на экране
 
 public:
+	void setFace(int pface)
+	{
+		face = pface;
+	}
+
+	// создаем метод сетерра (метод который записывает информацию в переменные класса)
+	void setPosition(int px, int py) // создаем функцию, назначение которой запись значений в переменные
+	{
+		// проверка размеров экрана (валидация переменных)
+		if (px >= 0 && px <= 79)
+		{
+			x = px;
+			
+		}
+		if (py >= 0 && py <= 24)
+		{
+			y = py;
+		}
+		
+	}
 	//создаем метод отображения перса на экране
 	void show()
 	{
-		x = 5;
-		y = 3;
 		gotoxy(x, y);
-		cout << face;
+		cout << (char)face;// преобразовываем переменную к символьному типу
 	};
+
+	// метод изменяет координаты
+	void move(int ch)
+	{
+		switch (ch)
+		{
+		case 77: // стрелка вправо
+			x++;
+			break;
+		case 72: // стрелка вверх
+			y--;
+			break;
+		case 80: // стрелка вниз
+			y++;
+			break;
+		case 75: // стрелка влево
+			x--;
+			break;
+		}
+	}
 };
 
 int main(int argc, char* argv[])
@@ -38,9 +75,23 @@ int main(int argc, char* argv[])
 
 	// создаем экземпляр класса
 	CHero hero; // переменная
+	
+	// задаем координаты перса и отображаем на экране
+	hero.setPosition(5, 3);
+	hero.setFace(1);
 
-	hero.show();
+	int ch;
+	
+	while (true)
+	{
+		ch = _getch();
 
+		hero.move(ch);
+
+		system("cls");
+
+		hero.show();
+	}
 	_getch();
 	return 0;
 }
